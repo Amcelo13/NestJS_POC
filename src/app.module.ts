39ccd122358d18d28d022fe982310ae82b45f8e5
chetelise <chetan.1129@zenmonk.tech>
2023-09-11 17:44:30 +0530

    sad

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,14 @@ import { SummaryModule } from "./summary/summary.module";
 import { ReportModule } from "./report/report.module";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose/dist/mongoose.module";
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
     SummaryModule,
     ReportModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
